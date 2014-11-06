e204bc3b75c594ee0357360db6e50e3ffa1187ee
ThatOneCloud <corn0317@hotmail.com>
2014-11-06 13:59:59 -0600

    Add ReportUI class

---
+++ src/ReportUI.java
@@ -0,0 +1,13 @@
+/**
+ * The front end user interface for report related actions.
+ * 
+ * @author Blake
+ *
+ */
+public class ReportUI {
+
+	public ReportUI() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
