609fd0e592bb5d710a1772eb7aff121d41ceda79
nubx <adamdefoe@hotmail.com>
2014-11-16 10:54:19 -0600

    made a checkpoint for comments > 100 characters

--- src/ServiceOccasionList.java
+++ src/ServiceOccasionList.java
@@ -23,6 +23,10 @@ public class ServiceOccasionList {
 		Date currDate = new Date();
 		DateFormat dateFormat = new SimpleDateFormat("MM-dd-yyyy HH-mm-ss");
 		
+		//check if comments string is too long
+		if(comments.length > 100)
+			comments = comments.substring(0, 99);
+		
 		//Create new service occasion
 		ServiceOccasion newOccasion = new ServiceOccasion(service, member, provider, date, dateFormat.format(currDate), comments);
 		
