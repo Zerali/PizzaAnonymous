606b894bb0c81a0369d24653c46eff9a51220da9
desilva2007 <amila.desilva20@yahoo.com>
2014-11-17 19:53:00 -0600

    MemberControllerTest

--- test/MemberControllerTest.java
+++ test/MemberControllerTest.java
@@ -17,16 +17,30 @@ public class MemberControllerTest {
 	public MemberController testMemberController = new MemberController();
 
 	@Test
+	/**
+	 * Test case for addMember() in the MemberController
+	 */
 	public void testAddMember() 
 	{
+		//The first time a member with unique details is added to the list
 		assertTrue(testMemberController.addMember("Tim", 11478, "9th Ave S.", "Saint Cloud", "MN", 56301, true));
 
+		//If the member exists, the member is not added to the list
 		assertFalse(testMemberController.addMember("Tim", 11478, "9th Ave S.", "Saint Cloud", "MN", 56301, true));
 	}
 
 	@Test
-	public void testEditMember() {
-		fail("Not yet implemented");
+	/**
+	 * Test case for deleteMember() in the MemberController
+	 */
+	public void testDeleteMember() 
+	{
+		
+		testMemberController.addMember("Mark", 15975, "10th Ave S.", "Saint Cloud", "MN", 56303, true);
+		
+		assertTrue(testMemberController.deleteMember(15975));
+		
+		assertFalse(testMemberController.deleteMember(57951));
 	}
 	
 	@Test
